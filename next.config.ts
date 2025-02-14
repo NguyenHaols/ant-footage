4ac23d4c6869aed9d7d59ba1f48c72225dd367a6
nguyenhaols <nguyenhaocu0@gmail.com>
2025-02-14 14:57:35 +0700

    update headers in next config

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,19 @@ const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
+    async headers() {
+        return [
+            {
+                source: '/:path*',
+                headers: [
+                    {
+                        key: 'Content-Security-Policy',
+                        value: 'upgrade-insecure-requests',
+                    },
+                ],
+            },
+        ];
+    },
     images: {
         remotePatterns: [
             {
